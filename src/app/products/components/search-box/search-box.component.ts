032236bf492134b12311f5a17560cd4e08f6f450
Hector Toro <alejotoro94@hotmail.com>
2024-03-04 18:56:39 -0500

    Empiezo a agregar lógica del search-box, en este momento obtengo el valor del input haciendo uso del decorados @ViewChild

--- src/app/products/components/search-box/search-box.component.ts
+++ src/app/products/components/search-box/search-box.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'product-search-box',
@@ -8,9 +8,24 @@ import { Component } from '@angular/core';
       type="text"
       placeholder="Buscar producto"
       class="form-control mt-2"
+      (keyup.enter)="searchProduct()"
+      #txtProduct
     >
   `,
 })
 export class SearchBoxComponent {
+
+  //!Uso viewChild
+  @ViewChild('txtProduct')
+  public product!: ElementRef<HTMLInputElement>;
+
+  //! Debemos traernos nuestro servicio para actualizar el valor
+  constructor() {
+
+  }
+
+  public searchProduct = (): void => {
+    console.log("Obtengo el valor deseado", this.product.nativeElement.value);
+  }
 
 }
